8fc16266519c9cea74ea582cc4bbe5cffe09062e
Makinwa Bukola <bukola.makinwa@andela.com>
2018-11-17 14:57:11 +0100

    intro. to react v4 from intro to carousel section

---
+++ src/Carousel.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active].value} alt="primary animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            /* ideally have an image in a button with the button having the click event*/
+            /* eslint-disable-next-line */
+            <img
+              onClick={this.handleIndexClick}
+              key={photo.value}
+              data-index={index}
+              src={photo.value}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;

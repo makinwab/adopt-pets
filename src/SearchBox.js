10c581d55106d310dbb6e53ac38bc7a72b5e11d8
Makinwa Bukola <bukola.makinwa@andela.com>
2018-11-17 17:20:21 +0100

    implement React Context

---
+++ src/SearchBox.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { ANIMALS } from "petfinder-client";
+import { Consumer } from "./SearchContext";
+
+class SearchBox extends React.Component {
+  render() {
+    return (
+      <Consumer>
+        {context => (
+          <div className="search-params">
+            <label htmlFor="location">
+              Location
+              <input
+                onChange={context.handleLocationChange}
+                type="text"
+                id="location"
+                value={context.location}
+                placeholder="location"
+              />
+            </label>
+
+            <label htmlFor="animal">
+              Animal
+              <select
+                id="animal"
+                value={context.animal}
+                onChange={context.handleAnimalChange}
+                onBlur={context.handleAnimalChange}
+              >
+                <option />
+                {ANIMALS.map(animal => (
+                  <option key={animal} value={animal}>
+                    {animal}
+                  </option>
+                ))}
+              </select>
+            </label>
+
+            <label htmlFor="breed">
+              Breed
+              <select
+                id="breed"
+                value={context.breed}
+                onChange={context.handleBreedChange}
+                onBlur={context.handleBreedChange}
+                disabled={context.breeds.length === 0}
+              >
+                <option />
+                {context.breeds.map(breed => (
+                  <option key={breed} value={breed}>
+                    {breed}
+                  </option>
+                ))}
+              </select>
+            </label>
+
+            <button>Submit</button>
+          </div>
+        )}
+      </Consumer>
+    );
+  }
+}
+
+export default SearchBox;
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,113 +1,11 @@
 import React from "react";
-import pf, { ANIMALS } from "petfinder-client";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import SearchBox from "./SearchBox";
 
 class SearchParams extends React.Component {
-  state = {
-    location: "Seattle, WA",
-    animal: "",
-    breed: "",
-    breeds: []
-  };
-
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  };
-
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value,
-        breed: ""
-      },
-      this.getBreeds
-    );
-  };
-
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed
-        .list({
-          animal: this.state.animal
-        })
-        .then(data => {
-          if (
-            data.petfinder &&
-            data.petfinder.breeds &&
-            Array.isArray(data.petfinder.breeds.breed)
-          ) {
-            this.setState({ breeds: data.petfinder.breeds.breed });
-          } else {
-            this.setState({ breeds: [] });
-          }
-        });
-    } else {
-      this.setState({ breeds: [] });
-    }
-  }
-
   render() {
     return (
-      <div className="search-params">
-        <label htmlFor="location">
-          Location
-          <input
-            onChange={this.handleLocationChange}
-            type="text"
-            id="location"
-            value={this.state.location}
-            placeholder="location"
-          />
-        </label>
-
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={this.state.animal}
-            onChange={this.handleAnimalChange}
-            onBlur={this.handleAnimalChange}
-          >
-            <option />
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={this.state.breed}
-            onChange={this.handleBreedChange}
-            onBlur={this.handleBreedChange}
-            disabled={this.state.breeds.length === 0}
-          >
-            <option />
-            {this.state.breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <button>Submit</button>
+      <div className="search-route">
+        <SearchBox />
       </div>
     );
   }
